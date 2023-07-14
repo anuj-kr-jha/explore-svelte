65289e0ea40b8d7c152e8747a05e989e5766a07b
Anuj Kumar Jha <anuj.kr.jha@protonmail.com>
2023-07-15 00:40:01 +0530

    1

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,21 @@
 <script lang="ts">
-  import src from './assets/svelte.svg';
-  import Nested from './Nested.svelte';
-	import HtmlTag from './HtmlTag.svelte';
-  let name = 'svelte';
+	let count1 = 0;
+	let count2 = 0;
+
+	function increment1() {
+		count1 += 1;
+	}
+	function increment2() {
+		count2 += 1;
+	}
 </script>
 
-<main>
-<p>welcome to svelte</p>
-<img src={src} alt={name} />
-<img {src} alt="svelte" /> <!--It's not uncommon to have an attribute where the name and value are the same, like src={src}. Svelte gives us a convenient shorthand for these cases-->
-</main>
+<button on:click={increment1}>
+	Clicked {count1}
+	{count1 === 1 ? 'time' : 'times'}
+</button>
 
-<Nested /> <!-- Notice that even though Nested.svelte has a <p> element, the styles from App.svelte don't leak in. -->
-
-<HtmlTag />
-
-<style>
-	p {
-		color: goldenrod;
-		font-family: 'Comic Sans MS', cursive;
-		font-size: 2em;
-	}
-</style>
+<button on:click={increment2}>
+	Clicked {count2}
+	{count2 === 1 ? 'time' : 'times'}
+</button>
