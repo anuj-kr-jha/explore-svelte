1ac3bb3c231d9e8c02c439f169350fc8be81b589
Anuj Kumar Jha <anuj.kr.jha@protonmail.com>
2023-07-15 00:54:16 +0530

    reactive declaration

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,5 @@
 <script lang="ts">
-	let count1 = 0;
-	let count2 = 0;
-
-	function increment1() {
-		count1 += 1;
-	}
-	function increment2() {
-		count2 += 1;
-	}
+	import ReactivityDeclaration from './components/1.reactivity.declaration.svelte';
 </script>
 
-<button on:click={increment1}>
-	Clicked {count1}
-	{count1 === 1 ? 'time' : 'times'}
-</button>
-
-<button on:click={increment2}>
-	Clicked {count2}
-	{count2 === 1 ? 'time' : 'times'}
-</button>
+<ReactivityDeclaration />
---
+++ src/components/1.reactivity.declaration.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	let count = 0;
+	// let double = count*2; // this will not update when count changes
+	// some parts of a component's state need to be computed from other parts for that we need to use reactive declarations
+
+	$: double = count * 2; // this will update when count changes
+
+	function increment() {
+		count += 1;
+	}
+
+</script>
+
+<button on:click={increment}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
+
+<p>{count} doubled is {double}</p>
+<!-- Of course, you could just write {count * 2} in the markup instead — you don't have to use reactive values. Reactive values become particularly valuable (no pun intended) when you need to reference them multiple times, or you have values that depend on other reactive values. -->
+<p>{count} doubled is {count*2}</p>
+
+<!-- Note that reactive declarations and statements will run after other script code and before component markup is rendered. -->
