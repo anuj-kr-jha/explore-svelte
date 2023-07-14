e5e46f324eb7139acd5ee76d975787743ed457c0
Anuj Kumar Jha <anuj.kr.jha@protonmail.com>
2023-07-15 00:26:14 +0530

    htmlTag

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import src from './assets/svelte.svg';
   import Nested from './Nested.svelte';
+	import HtmlTag from './HtmlTag.svelte';
   let name = 'svelte';
 </script>
 
@@ -10,8 +11,9 @@
 <img {src} alt="svelte" /> <!--It's not uncommon to have an attribute where the name and value are the same, like src={src}. Svelte gives us a convenient shorthand for these cases-->
 </main>
 
-
 <Nested /> <!-- Notice that even though Nested.svelte has a <p> element, the styles from App.svelte don't leak in. -->
+
+<HtmlTag />
 
 <style>
 	p {
