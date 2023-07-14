8d46ba46b404130109a858c1f10ec582463d9c26
Anuj Kumar Jha <anuj.kr.jha@protonmail.com>
2023-07-15 00:07:53 +0530

    combining components

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
-
+  import src from './assets/svelte.svg';
+  import Nested from './Nested.svelte';
+  let name = 'svelte';
 </script>
 
 <main>
 <p>welcome to svelte</p>
+<img src={src} alt={name} />
+<img {src} alt="svelte" /> <!--It's not uncommon to have an attribute where the name and value are the same, like src={src}. Svelte gives us a convenient shorthand for these cases-->
 </main>
 
-<style>
 
+<Nested /> <!-- Notice that even though Nested.svelte has a <p> element, the styles from App.svelte don't leak in. -->
+
+<style>
+	p {
+		color: goldenrod;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
 </style>
