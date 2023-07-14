736479814c456a08753420d88f49a68f63f3bf2e
Anuj Kumar Jha <anuj.kr.jha@protonmail.com>
2023-07-15 01:04:14 +0530

    reactive statements

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import ReactivityDeclaration from './components/1.reactivity.declaration.svelte';
+	import ReactivityStatements from './components/2.reactivity.statements.svelte';
 </script>
 
 <ReactivityDeclaration />
+<ReactivityStatements />
---
+++ src/components/2.reactivity.statements.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	let count = 0;
+
+	function increment() {
+		count += 1;
+	}
+
+	// We're not limited to declaring reactive values — we can also run arbitrary statements reactively. For example, we can log the value of count whenever it changes:
+	$: console.log(`the count is ${count}`);
+
+	// We can easily group statements together with a block:
+	$: {
+		console.log(`block: the count is ${count}`);
+		console.log(`block: this will also be logged whenever count changes`);
+  }
+
+
+	// We can even put the $: in front of things like if blocks:
+	$: if (count >= 10) {
+		console.log('count is dangerously high!');
+	}
+
+</script>
+
+
+<button on:click={increment}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
